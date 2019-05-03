06e603c1c540279af92b8096a65baadb747b9c73
simoncobb1966 <simoncobb1966@gmail.com>
2019-05-03 13:42:52 +0100

    got a modal & buttons working with material-ui

--- src/components/clearbutton.js
+++ src/components/clearbutton.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class Clearbutton extends Component {
+
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = (buttonName) => {
+        this.setState({ open: false });
+        this.props.buttonHandlerFunction(buttonName.currentTarget.value)
+    };
+
 
     clearButtonText = () => {
         if (this.props.numOfTasks === 1) {
@@ -22,10 +42,36 @@ class Clearbutton extends Component {
 
     render() {
         return (
+
+
             <div className={this.clearButtonClass()}>
                 <div className="btn btn-primary notifications">
-                    <button type="button" onClick={() => this.props.buttonHandlerFunction("clear")} className="btn btn-primary addbutton" data-toggle="tooltip" data-placement="bottom" title="Clears the whole list">{this.clearButtonText()}</button>
+                    <button type="button" onClick={this.handleClickOpen} className="btn btn-primary addbutton" data-toggle="tooltip" data-placement="bottom" title="Clears the whole list">{this.clearButtonText()}</button>
+                    {/* <button type="button" onClick={() => this.props.buttonHandlerFunction("clear")} className="btn btn-primary addbutton" data-toggle="tooltip" data-placement="bottom" title="Clears the whole list">{this.clearButtonText()}</button>
+                 */}
                 </div>
+
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Clear All Tasks?"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Are you sure you want to clear all tasks?
+                  </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} value="cancel" color="primary" autoFocus>
+                            Cancel
+                  </Button>
+                        <Button onClick={this.handleClose} value="clear" color="primary" >
+                            Clear
+                  </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         )
     }
