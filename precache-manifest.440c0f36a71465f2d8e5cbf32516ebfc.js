432d1d3dcd006c4dbeb66dbe8fee1345df28405d
simoncobb1966 <simoncobb1966@gmail.com>
2019-06-13 11:56:06 +0100

    Updates

---
+++ precache-manifest.440c0f36a71465f2d8e5cbf32516ebfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a478fef64b38cfc2c4b3",
+    "url": "/todo-app-frontend/static/js/runtime~main.52db614d.js"
+  },
+  {
+    "revision": "28547d6787633fd3ea9b",
+    "url": "/todo-app-frontend/static/js/main.d58c98d4.chunk.js"
+  },
+  {
+    "revision": "3775483fa4365626421a",
+    "url": "/todo-app-frontend/static/js/2.8e07a964.chunk.js"
+  },
+  {
+    "revision": "28547d6787633fd3ea9b",
+    "url": "/todo-app-frontend/static/css/main.e549b86b.chunk.css"
+  },
+  {
+    "revision": "6f5c77b4a0e4835e16b293471991e232",
+    "url": "/todo-app-frontend/index.html"
+  }
+];
